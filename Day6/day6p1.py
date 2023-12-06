d15e7077be5bc64a1598e10ad52a871b1c1d0ae2
Justin Schreiber <jschreiber234@gmail.com>
2023-12-06 15:38:54 -0500

    day6 pt2 brute force completed

--- Day6/day6p1.py
+++ Day6/day6p1.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-#puzzle_input_file_path = Path(__file__).with_name('example.txt')
-puzzle_input_file_path = Path(__file__).with_name('input.txt')
+puzzle_input_file_path = Path(__file__).with_name('example.txt')
+#puzzle_input_file_path = Path(__file__).with_name('input.txt')
 puzzle_input_file = open(puzzle_input_file_path,'r')
 puzzle_input = puzzle_input_file.read().splitlines() #can stay string 
 puzzle_input_file.close()
@@ -17,13 +17,38 @@ ways=[]
 for i in range(len(times)):
     ways.append(0)
 
+#attempt at binary search
+# #low = 1 high = time?
+# for i in range(len(times)):
+#     low = 1
+#     high = times[i]
+#     while low <= high:
+#         mid = (low + high) // 2
+#         if ((times[i] - low) * low) > distances[i]:
+#             # print(low)
+#             ways[i]+=1
+#             low+=1
+#         elif (((times[i] - high) * high) < distances[i]) or ((times[i] - low) * low) > distances[i]:
+#             high = mid -1 
+#         elif ((times[i] - low) * low) <= distances[i]:
+#             print(low)
+#             low+=1
+
+#         # elif ((times[i] - low) * low) == distances[i]:
+#         #      low = mid +1 
+#     print('dddddddddddddddddddddddddddddd')
+
+for i in range(len(times)):
+    ways.append(0)
+
 for i in range(len(times)):
     for j in range(1,times[i]+1):
         if (times[i] - j) * j > distances[i]:
             ways[i]+=1
+        
 product = 1
 
 for way in ways:
     product*=way
-print(product)
+print(ways)
 
---
+++ Day6/day6pt2.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+#puzzle_input_file_path = Path(__file__).with_name('example.txt')
+puzzle_input_file_path = Path(__file__).with_name('input.txt')
+puzzle_input_file = open(puzzle_input_file_path,'r')
+puzzle_input = puzzle_input_file.read().splitlines() #can stay string 
+puzzle_input_file.close()
+
+times = puzzle_input[0].split(':')[1].split()
+times = [''.join(times)] 
+times = list(map(int, times))
+
+distances = puzzle_input[1].split(':')[1].split()
+distances = [''.join(distances)] 
+distances = list(map(int, distances))
+
+ways=[]
+for i in range(len(times)):
+    ways.append(0)
+
+
+
+for i in range(len(times)):
+    for j in range(1,times[i]+1):
+        if (times[i] - j) * j > distances[i]:
+            ways[i]+=1
+        
+product = 1
+
+for way in ways:
+    product*=way
+print(product)
